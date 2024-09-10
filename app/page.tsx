bf56ac2083f820f8d79b3442c9109da41c2e8116
PhamHuyHiep45200 <99460571+PhamHuyHiep45200@users.noreply.github.com>
2024-09-10 14:00:31 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <Link href="mywallet://?foo=bar">open app mywallet with link</Link>
+      <Link href="jrewallet://?clientId=client&callbackUrl=calbee://&key=asdf-ghfg-tyug-jcvfk">open app mywallet with link</Link>
       <br />
-      <a href="fb://">Open my app</a>
+      <a href="jrewallet://?clientId=client&callbackUrl=calbee://&key=asdf-ghfg-tyug-jcvfk">Open my app</a>
       <br />
       <a href="https://play.google.com/store/apps/details?id=com.wallet.crypto.trustapp">
         trustapp
